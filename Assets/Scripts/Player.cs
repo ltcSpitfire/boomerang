883012004ff5b10eac33bc4944986c3045c5e0e3
ltcSpitfire <ltc.spitfire@gmail.com>
2021-02-05 15:30:40 +0200

    Climbing Slopes
    Edited capsule collider, new physics material 2D and updated player script.
    
    At this time: Player slides a little on slopes still

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,30 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    // Config
+    // Config Variables
     [SerializeField] float runSpeed = 5f;
     [SerializeField] float jumpStrength = 5f;
+    [SerializeField] PhysicsMaterial2D zeroFriction;
+    [SerializeField] PhysicsMaterial2D maxFriction;
+    [SerializeField] float slopeCheckDistance;
+    [SerializeField] LayerMask whatIsGround;
 
-    // State
+    // Private Variables
+    private Vector2 colliderSize;
+    private float slopeDownAngle;
+    private float slopeSideAngle;
+    private float lastSlopeAngle;
+    private bool isOnSlope;
+    private Vector2 slopeNormalPerp;
+    private float controlThrow;
+
+    // States
     bool isAlive = true;
 
     // Cached component references
     Rigidbody2D myRigidBody;
     Animator myAnimator;
-    Collider2D myCollider2D;
+    CapsuleCollider2D myCollider2D;
 
 
     // Start is called before the first frame update
@@ -23,7 +36,9 @@ public class Player : MonoBehaviour
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
-        myCollider2D = GetComponent<Collider2D>();
+        myCollider2D = GetComponent<CapsuleCollider2D>();
+
+        colliderSize = myCollider2D.size;
     }
 
     // Update is called once per frame
@@ -31,12 +46,13 @@ public class Player : MonoBehaviour
     {
         Run();
         Jump();
+        SlopeCheck();
         FlipSprite();
     }
 
     private void Run()
     {
-        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
+        controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
 
@@ -56,6 +72,72 @@ public class Player : MonoBehaviour
         {
             Vector2 jumpVelocityToAdd = new Vector2(0f, jumpStrength);
             myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void SlopeCheck()
+    {
+        Vector2 checkPos = transform.position - new Vector3(0.0f, colliderSize.y / 2);
+
+        SlopeCheckHorizontal(checkPos);
+        SlopeCheckVertical(checkPos);
+    }
+
+    private void SlopeCheckHorizontal(Vector2 checkPos)
+    {
+        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, whatIsGround);
+        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, whatIsGround);
+
+        if (slopeHitFront)
+        {
+            isOnSlope = true;
+
+            slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+
+        }
+        else if (slopeHitBack)
+        {
+            isOnSlope = true;
+
+            slopeSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+        else
+        {
+            slopeSideAngle = 0.0f;
+            isOnSlope = false;
+        }
+    }
+
+    private void SlopeCheckVertical(Vector2 checkPos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, whatIsGround);
+
+        if (hit)
+        {
+
+            slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
+
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if (slopeDownAngle != lastSlopeAngle)
+            {
+                isOnSlope = true;
+            }
+
+            lastSlopeAngle = slopeDownAngle;
+
+            Debug.DrawRay(hit.point, slopeNormalPerp, Color.red);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+
+        }
+
+        if (isOnSlope && controlThrow == 0.0f)
+        {
+            myRigidBody.sharedMaterial = maxFriction;
+        }
+        else
+        {
+            myRigidBody.sharedMaterial = zeroFriction;
         }
     }
 
