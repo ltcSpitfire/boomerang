7a75459c62a98df91e97b13ff472abef5bd59b68
s0ze1 <77828411+s0ze1@users.noreply.github.com>
2021-02-14 06:01:40 +0200

    deleted sample scene, added a new scene and implemented simple scene change (still some problems)
    Scene change still has some problems and new scene is just there to test the scene changing for now

---
+++ Assets/Scripts/LevelLoaderScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoaderScript : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+}
