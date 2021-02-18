1572c7798ab4dbc40aff1d508b6edfbf351c8d0c
ltcSpitfire <ltc.spitfire@gmail.com>
2021-02-18 19:57:02 +0200

    Boomerang mechanics
    Temporary sprite added, boomerang spawn and aim locations placed, boomerang can collide with ground and be stood on.

---
+++ Assets/Boomerang.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boomerang : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool hasHitForeground;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasHitForeground == false)
+        {
+            float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
+    }
+
+    //Checks if the boomerang has hit the foreground layer then freezes the object
+    private void OnCollisionEnter2D(Collision2D collision) //note to self: add check for tag later for dealing with enemies
+    {
+        hasHitForeground = true;
+        rb.velocity = Vector2.zero;
+        rb.isKinematic = true;
+    }
+}
---
+++ Assets/Scripts/BoomerangThrow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerangThrow : MonoBehaviour
+{
+    public GameObject boomerang;
+    public float throwForce;
+    public Transform throwPoint; //Where the boomerang spawns
+    public Transform aimPosition; //The direction we want the boomerang to go
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 throwPos = transform.position;
+        Vector2 aimPos = aimPosition.transform.position;
+        Vector2 throwDirection = aimPos - throwPos;
+        transform.right = throwDirection;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            ThrowBoomerang();
+        }
+    }
+
+    private void ThrowBoomerang()
+    {
+        GameObject newBoomerang = Instantiate(boomerang, throwPoint.position, throwPoint.rotation);
+        newBoomerang.GetComponent<Rigidbody2D>().velocity = transform.right * throwForce;
+    }
+}
