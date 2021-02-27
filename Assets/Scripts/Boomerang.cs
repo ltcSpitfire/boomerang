a26775fde5d992111c7aef79fad5cd6584ad83bd
ltcSpitfire <ltc.spitfire@gmail.com>
2021-02-27 03:33:05 +0200

    Hitting enemy
    Boomerang now returns to the player when it hits an enemy

--- Assets/Scripts/Boomerang.cs
+++ Assets/Scripts/Boomerang.cs
@@ -20,6 +20,7 @@ public class Boomerang : MonoBehaviour
     public bool movingToFinalTarget;
     public bool hitGround;
     public bool canBeDestroyed;
+    public bool hasHitEnemy;
 
     // Start is called before the first frame update
     void Start()
@@ -44,46 +45,52 @@ public class Boomerang : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!movingToSecondTarget && !hitGround)
+        if (!hasHitEnemy)
         {
-            transform.position = Vector2.MoveTowards(transform.position, firstTarget, throwForce * Time.deltaTime);
-
-            if (transform.position.x == firstTarget.x && transform.position.y == firstTarget.y)
+            if (!movingToSecondTarget && !movingToFinalTarget && !hitGround)
             {
-                movingToSecondTarget = true;
-                canBeDestroyed = true;
+                Debug.Log("Boomerang searching for first target");
+                transform.position = Vector2.MoveTowards(transform.position, firstTarget, throwForce * Time.deltaTime);
+
+                if (transform.position.x == firstTarget.x && transform.position.y == firstTarget.y)
+                {
+                    movingToSecondTarget = true;
+                    canBeDestroyed = true;
+                }
+            }
+
+            if (movingToSecondTarget && !hitGround)
+            {
+                Debug.Log("Boomerang searching for second target");
+                gameObject.layer = 8;
+                //rb.isKinematic = true;
+                transform.position = Vector2.MoveTowards(transform.position, secondTarget, throwForce * Time.deltaTime);
+
+                if (transform.position.x == secondTarget.x && transform.position.y == secondTarget.y)
+                {
+                    movingToSecondTarget = false;
+                    movingToFinalTarget = true;
+                    canBeDestroyed = true;
+                }
+            }
+
+            if (movingToFinalTarget && !hitGround)
+            {
+                Debug.Log("Boomerang searching for final target");
+                gameObject.layer = 11;
+                //rb.isKinematic = false;
+                transform.position = Vector2.MoveTowards(transform.position, throwPoint.position, throwForce * Time.deltaTime);
             }
         }
 
-        if (movingToSecondTarget && !hitGround)
+        if (hasHitEnemy)
         {
-            gameObject.layer = 8;
-            rb.isKinematic = true;
-            transform.position = Vector2.MoveTowards(transform.position, secondTarget, throwForce * Time.deltaTime);
-
-            if (transform.position.x == secondTarget.x && transform.position.y == secondTarget.y)
-            {
-                movingToSecondTarget = false;
-                movingToFinalTarget = true;
-                canBeDestroyed = true;
-            }
-        }
-
-        if (movingToFinalTarget && !hitGround)
-        {
-            gameObject.layer = 11;
-            rb.isKinematic = false;
             transform.position = Vector2.MoveTowards(transform.position, throwPoint.position, throwForce * Time.deltaTime);
-
-            if (transform.position.x == throwPoint.position.x && transform.position.y == throwPoint.position.y)
-            {
-                movingToFinalTarget = false;
-                DestroyBoomerang();
-            }
         }
 
         if (Input.GetMouseButtonDown(1) && hitGround)
         {
+            Debug.Log("Boomerang recalled");
             DestroyBoomerang();
         }
     }
@@ -93,6 +100,7 @@ public class Boomerang : MonoBehaviour
     {
         if (collision.gameObject.tag == "Ground")
         {
+            Debug.Log("Hit ground");
             hitGround = true;
             rb.velocity = Vector2.zero;
             rb.isKinematic = true;
@@ -102,20 +110,24 @@ public class Boomerang : MonoBehaviour
 
         if (collision.gameObject.tag == "Enemy")
         {
-            movingToFinalTarget = true;
+            Debug.Log("Hit enemy");
+            hasHitEnemy = true;
+            canBeDestroyed = true;
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Throw Point") && canBeDestroyed == true)
+        if (other.gameObject.tag == "Throw Point" && canBeDestroyed == true)
         {
+            Debug.Log("Boomerang returned");
             DestroyBoomerang();
         }
     }
 
     private void DestroyBoomerang()
     {
+        Debug.Log("Boomerang destroyed");
         Destroy(gameObject);
         bThrower.created = false;
     }
