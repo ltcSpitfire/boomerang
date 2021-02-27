fc6a46041f2b06bca1265f0d24e331ff76f1d47d
ltcSpitfire <ltc.spitfire@gmail.com>
2021-02-27 22:28:31 +0200

    Enemy snake fix
    Fixed a bug that stopped caused the snake to be stuck in corners after losing aggro towards the player.

--- Assets/Scripts/EnemySnake.cs
+++ Assets/Scripts/EnemySnake.cs
@@ -8,7 +8,8 @@ public class EnemySnake : MonoBehaviour
 
     public GameObject player;
     private Transform playerPos;
-    private Vector2 spawnPos;
+    private Vector2 lastPos;
+    public float moveThreshold = 0f;
     private float distanceToPlayer;
     public float aggroRange;
     public bool hasAggro;
@@ -17,6 +18,19 @@ public class EnemySnake : MonoBehaviour
 
     Rigidbody2D myRigidBody;
 
+    private float noMovementThreshold = 0.0001f;
+    private const int noMovementFrames = 5;
+    Vector2[] previousLocations = new Vector2[noMovementFrames];
+    private bool isMoving;
+
+    void Awake()
+    {
+        for (int i = 0; i < previousLocations.Length; i++)
+        {
+            previousLocations[i] = Vector2.zero;
+        }
+    }
+
     // Use this for initialization
     void Start()
     {
@@ -24,19 +38,41 @@ public class EnemySnake : MonoBehaviour
         triggerExitActivated = false;
         myRigidBody = GetComponent<Rigidbody2D>();
         playerPos = player.GetComponent<Transform>();
-        spawnPos = GetComponent<Transform>().position;
+        //spawnPos = GetComponent<Transform>().position;
         renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(myRigidBody.velocity);
+        for (int i = 0; i < previousLocations.Length - 1; i++)
+        {
+            previousLocations[i] = previousLocations[i + 1];
+        }
+        previousLocations[previousLocations.Length - 1] = transform.position;
+
+        for (int i = 0; i < previousLocations.Length - 1; i++)
+        {
+            if (Vector3.Distance(previousLocations[i], previousLocations[i + 1]) >= noMovementThreshold)
+            {
+                //The minimum movement has been detected between frames
+                isMoving = true;
+                break;
+            }
+            else
+            {
+                isMoving = false;
+            }
+        }
+
+
+
+        //Debug.Log(transform.position);
         distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
 
         if (distanceToPlayer < aggroRange)
         {
-            Debug.Log("Enemy has aggro");
+            //Debug.Log("Enemy has aggro");
             hasAggro = true;
         }
         else
@@ -46,23 +82,20 @@ public class EnemySnake : MonoBehaviour
 
         if (hasAggro)
         {
+            //Debug.Log("Chasing player");
             ChasePlayer();
         }
         else
         {
+            //Debug.Log("Patrolling");
             Patrol();
         }
 
-        if (!hasAggro && triggerExitActivated)
+        if (!isMoving && !hasAggro) //Fail-safe to stop enemy being stuck after losing aggro
         {
-            triggerExitActivated = false;
-            Flip();
+            transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
         }
-    }
-    public void Flip() //Fail-safe if snake gets stuck after losing aggro
-    {
-        transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
-        Debug.Log("Trigger flipped");
+
     }
 
     bool IsFacingLeft()
@@ -74,32 +107,40 @@ public class EnemySnake : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
-            Debug.Log("Trigger exited ground");
-            transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
             triggerExitActivated = true;
+
+            if (triggerExitActivated)
+            {
+                Debug.Log("Trigger exited ground");
+                transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
+                Debug.Log("Trigger set to false");
+                triggerExitActivated = false;
+            }
         }
     }
 
     private void Patrol()
     {
         renderer.color = new Color(1, 1, 1, 1); //normal colour when not chasing the player
-        //triggerExitActivated = false;
+
         if (IsFacingLeft())
         {
             //Debug.Log("Enemy is moving left");
             myRigidBody.velocity = new Vector2(-moveSpeed, 0f);
+            transform.localScale = new Vector2(-1, 1);
         }
         else
         {
             //Debug.Log("Enemy is moving right");
             myRigidBody.velocity = new Vector2(moveSpeed, 0f);
+            transform.localScale = new Vector2(1, 1);
         }
     }
 
     private void ChasePlayer()
     {
         renderer.color = new Color(1, 0, 0, 1); //red colour when chasing the player
-        //triggerExitActivated = false;
+
         if (transform.position.x < player.transform.position.x)
         {
             myRigidBody.velocity = new Vector2(moveSpeed, 0); //enemy is to the left side of player, move enemy right
@@ -110,15 +151,5 @@ public class EnemySnake : MonoBehaviour
             myRigidBody.velocity = new Vector2(-moveSpeed, 0); //enemy is to the right side of player, move enemy left
             transform.localScale = new Vector2(-1, 1);
         }
-
-        /*if (Vector2.Distance(transform.position, playerPos.position) < distanceToPlayer)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, playerPos.position, moveSpeed * Time.deltaTime);
-        }
-        else
-        {
-            transform.position = Vector2.MoveTowards(transform.position, spawnPos, moveSpeed * Time.deltaTime);
-            hasAggro = false;
-        }*/
     }
 }
